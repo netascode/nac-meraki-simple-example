ec9e406b9f592ac9fdfe25a99bcd48a2d8b33639
Jon <jonhumph@cisco.com>
2025-08-04 16:15:23 +0100

    removing provider example from main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 1.9"
-  required_providers {
-    meraki = {
-      source  = "CiscoDevNet/meraki"
-      version = ">= 1.6.0"
-    }
-  }
-}
-
 module "meraki" {
   source  = "netascode/nac-meraki/meraki"
   version = "0.3.2"
