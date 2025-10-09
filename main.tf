265aafe6e9d17dc7085bb44e8cd8f31d75f25e1c
Jon <jonhumph@cisco.com>
2025-10-09 19:57:54 +0100

    Remove version and additional yaml_directories from meraki module configuration

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 module "meraki" {
   source  = "netascode/nac-meraki/meraki"
-  version = "0.3.3"
 
-  yaml_directories = ["data", "defaults"]
-  write_model_file = "./merged_defaults.yaml"
+  yaml_directories = ["data"]
 }
