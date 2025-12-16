281c3a80599cdcc57bad00a1875e7f595134f22b
jon-humphries <jonhumph@cisco.com>
2025-12-16 13:00:29 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    meraki = {
-      source  = "CiscoDevNet/meraki"
-      version = ">= 1.9.0"
-    }
-  }
-}
-
-provider "meraki" {}
-
 module "meraki" {
   source  = "netascode/nac-meraki/meraki"
   version = "0.6.0"
